0dee628b1d1cba91f3b810515f94c17d83c88c1f
El Bachir DRISSI <elbachir.drissi2003@gmail.com>
2024-12-03 21:18:32 +0100

    yaarbi

--- CHATBOT_CBAHI/CHATBOT_CBAHI/settings.py
+++ CHATBOT_CBAHI/CHATBOT_CBAHI/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
+import os, sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Ajouter le chemin racine
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
